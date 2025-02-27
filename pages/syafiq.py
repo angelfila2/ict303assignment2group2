c25dd02f1a69fdaa6be787a8261a8642a0f30630
angelfila2 <127929465+angelfila2@users.noreply.github.com>
2025-02-27 16:52:37 +0800

    Update syafiq.py

--- pages/syafiq.py
+++ pages/syafiq.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pathlib import Path
 
 # ===========================
 # Streamlit Page Configuration
