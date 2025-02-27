f641093e759688ff6ee6c26e66654b76ecea3469
JustPlaySimple <simplicityjiepi@hotmail.com>
2025-02-27 16:40:54 +0800

    Update damian.py

--- pages/damian.py
+++ pages/damian.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("📊Damian Page pn")
 st.write("This page loads and processes GDP data.")
