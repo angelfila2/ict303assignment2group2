7be38115c200712631dcd2004f5bde0fa418e223
angelfila2 <127929465+angelfila2@users.noreply.github.com>
2025-03-13 15:45:47 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Analysis of healthcare expenditure and its imapct",
+    page_icon="🌍",
+    layout="wide"
+)
+
+st.title("Welcome to the GDP Dashboard")
+
+
+
+
+
+st.write("Select a page from the sidebar to begin your analysis.")
