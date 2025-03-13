03497e6fde478ceaacce2bbc5da5ab4c796d6fca
angelfila2 <127929465+angelfila2@users.noreply.github.com>
2025-03-13 16:04:17 +0800

    Update life_expectancy.py

--- pages/life_expectancy.py
+++ pages/life_expectancy.py
@@ -14,7 +14,7 @@ def load_data(file_path):
         st.error("Error: 'ASEAN average life expectancy.xlsx' not found. Did you upload it?")
         return None
 
-df = load_data("/data/ASEAN average life expectancy.xlsx")
+df = load_data("data/ASEAN average life expectancy.xlsx")
 
 if df is not None:
     # --- Website Content ---
