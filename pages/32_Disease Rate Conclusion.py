8be8c23a1b982318e18f4aa49bd69f617ce2eccd
JustPlaySimple <simplicityjiepi@hotmail.com>
2025-03-31 03:06:00 +0800

    Update 32_Disease Rate Conclusion.py

--- pages/32_Disease Rate Conclusion.py
+++ pages/32_Disease Rate Conclusion.py
@@ -6,7 +6,8 @@ st.title("📌 Conclusion")
 st.markdown("""
 ### In a Nutshell  
 Our research points out the effectiveness of **strategic immunization investment** as a means of containing infectious diseases.  
-A decrease in disease cases **usually correlates** with higher vaccination spending, but the true effect depends on **how efficiently resources are used** rather than just the amount of money spent.
+
+Decrease in disease cases **often correlates** with higher vaccination spending, but the true effect depends on **how efficiently resources are used** rather than just the amount of money spent.
 """)
 
 # Key Takeaways
@@ -15,13 +16,18 @@ st.subheader("🔑 Key Takeaways for this Research")
 st.markdown("""
 - **Effective Resource Allocation Matters:**  
   Even though numerous countries spend a lot of money on vaccination, some, like **Myanmar**, have seen notable drops in disease cases with **relatively modest expenditure**.  
+  
   This indicates that targeted intervention strategies and **efficient allocation** can occasionally outperform sheer spending volume.
 
 - **Learning from Myanmar:**  
   Myanmar's was able to show impressive outcomes can also be achieved with a well-thought-out immunization strategy that emphasizes **community engagement, streamlined supply chains, and robust vaccination outreach**.  
-  Myanmar was able to **effectively drive down disease prevalence** despite spending considerably fewer resources than some of its regional peers, making it a **Role Model** for resource optimization.
 
-- **A Call to Action for Singapore’s Healthcare Policymakers:**  
+  Despite spending considerably fewer resources than some of its regional peers, Myanmar was able to **effectively drive down disease prevalence** , making their own country a **Role Model** for others in resource optimization.
+""")
+
+st.subheader("🚀 Call to Action for Singapore’s Healthcare Policymakers")
+
+st.markdown("""
   For healthcare policymakers in **Singapore**, these insights are particularly persuasive. By examining and follow behinds Myanmar's footstep, Singapore can consider:
 
   ✅ **Adopting more efficient vaccination distribution method** such as having more targeted community interventions.  
@@ -29,5 +35,8 @@ st.markdown("""
   ✅ **Optimizing existing resources** to further reduce the burden of infectious diseases.  
 
 Ultimately, immunization is just merely an expense—it is a **critical investment in public health and economic stability**.  
+""")
+
+st.markdown("""
 By **learning from successful Role Models** such as Myanmar, and tailoring these strategies to local context, Singapore could **strengthen its healthcare framework** and **safeguard its population** against preventable diseases.
 """)
